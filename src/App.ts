054937a9a1d7c422d986bb361ef0a875bc16c154
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2023-05-14 23:59:03 -0300

    d

--- src/App.ts
+++ src/App.ts
@@ -4,7 +4,7 @@ console.time('tempo de execução');
 
 
 
-const grafo = Grafo.gerarGrafoAleatorio(1000, 0.3);
+const grafo = Grafo.gerarGrafoAleatorio(100, 0.3);
 
 
 let matriz = grafo.gerarMatriz();
@@ -14,27 +14,27 @@ console.log("Matriz de Adjacencia")
 const fechosTransitivos = grafo.fechoTransitivoWarshall(matriz);
 console.log("Matriz de Fechos Transitivos - Warshall")
 
-const baseWarshall = grafo.gerarBase(fechosTransitivos);
-const antiBaseWarshall = grafo.gerarAntiBase(fechosTransitivos);
+// const baseWarshall = grafo.gerarBase(fechosTransitivos);
+// const antiBaseWarshall = grafo.gerarAntiBase(fechosTransitivos);
 
-console.log("Vertices da Base do Grafo a partir da matriz de Warshall");
-console.log(baseWarshall);
+// console.log("Vertices da Base do Grafo a partir da matriz de Warshall");
+// console.log(baseWarshall);
 
-console.log("Vertices da AntiBase do Grafo a partir da matriz de Warshall");
-console.log(antiBaseWarshall);
+// console.log("Vertices da AntiBase do Grafo a partir da matriz de Warshall");
+// console.log(antiBaseWarshall);
 
-const bfsFechosTransitivosDiretos = grafo.fechoTransitivoNaive();
-console.log("Fechos Transitivos - Naive");
+// const bfsFechosTransitivosDiretos = grafo.fechoTransitivoNaive();
+// console.log("Fechos Transitivos - Naive");
 
 
-const baseNaive = grafo.gerarBase(bfsFechosTransitivosDiretos);
-const antiBaseNaive = grafo.gerarAntiBase(bfsFechosTransitivosDiretos);
+// const baseNaive = grafo.gerarBase(bfsFechosTransitivosDiretos);
+// const antiBaseNaive = grafo.gerarAntiBase(bfsFechosTransitivosDiretos);
 
-console.log("Vertices da Base do Grafo a partir da matriz de Naive");
-console.log(baseNaive);
+// console.log("Vertices da Base do Grafo a partir da matriz de Naive");
+// console.log(baseNaive);
 
-console.log("Vertices da AntiBase do Grafo a partir da matriz de Naive");
-console.log(antiBaseNaive);
+// console.log("Vertices da AntiBase do Grafo a partir da matriz de Naive");
+// console.log(antiBaseNaive);
 
 
 
